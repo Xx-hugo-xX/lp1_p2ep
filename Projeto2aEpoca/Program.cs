8629de284b06e53cf264e6c33f763ff696e9195d
Hugo Feliciano <hugo.feliciano666@gmail.com>
2019-06-19 17:00:50 +0100

    Add project files.

---
+++ Projeto2aEpoca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto2aEpoca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
