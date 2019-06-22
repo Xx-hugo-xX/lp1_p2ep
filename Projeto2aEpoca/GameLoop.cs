ad7fcfe6de61ac790f47339d6dff16baaa9b3040
Pedro Fernandes <pedromafernandes@outlook.pt>
2019-06-22 21:12:15 +0100

    Adicionar GameLoop (de acordo com primeira fase)

---
+++ Projeto2aEpoca/GameLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto2aEpoca
+{
+    class GameLoop
+    {
+        bool play = true;
+        public void PlayGame(Board board, Renderer renderer)
+        {
+            while (play)
+            {
+                renderer.ShowGameValues(board);
+                renderer.DrawMap(board);
+                Console.ReadLine();
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    play = false;
+                }
+            }
+        }
+    }
+}
