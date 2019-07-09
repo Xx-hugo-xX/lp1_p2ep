a4a538c88ea76884c01f48eedd8ad527d4fd11cb
Rita Simões Saraiva <belgas2000@gmail.com>
2019-07-09 01:00:41 +0100

    Apagar classe Enemy, Adicionar ScoreComparer e adicionar metodos de HighScore na classe Game

---
+++ Projeto2aEpoca/ScoreComparer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto2aEpoca
+{
+    class ScoreComparer : IComparer<HighScore>
+    {
+        public int Compare(HighScore highScore1, HighScore highScore2)
+        {
+            return highScore2.Score.CompareTo(highScore1.Score);
+        }
+    }
+}
+
